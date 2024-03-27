f6a73a307418128c7951b75b4b16008f42e12eaf
Stephen <stephensellers034@Stephens-MacBook-Air.local>
2024-03-27 12:59:30 -0400

    favorite number

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 34;
+            Console.WriteLine("Guess my favorite number between 1 and 1000:");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput >= 34)
+            {
+                Console.WriteLine("No, No, No your guess is to high");
+            }
+            else if (userInput == 34)
+            {
+                Console.WriteLine("Ding, Ding, Ding, You are Correct!!!");
+            }
+            else 
+            {
+                Console.WriteLine("Ah, Ah, Ah to low");
+            }
+
+
         }
     }
 }
